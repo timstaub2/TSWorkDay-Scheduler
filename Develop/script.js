526d5baa417d493b55933d39829eed35d8178538
Timothy Staub <timstaub41@gmail.com>
2022-05-07 20:36:55 -0400

    fixed the localstorage

--- Develop/script.js
+++ Develop/script.js
@@ -1,10 +1,11 @@
+
 // new js file
 var now = moment().format('MMMM Do YYYY');
 $('#currentDay').text(now);
 
 //set time to var
 for (var i = 7; i < 17; i++) {
-    $(".hour"+i).text( moment(i, "LT").format("LT"))
+    $(".hour" + i).text(moment(i, "LT").format("LT"))
 }
 
 // check for time change and set background
@@ -25,7 +26,7 @@ for (var i = 7; i < 17; i++) {
 
 function textSave() {
     for (var i = 7; i < 17; i++) {
-        localStorage.setItem("userInput"+i, $(".text"+i));
+        localStorage.setItem("userInput"+i, $(".text"+i).val());
     }
 }
 
@@ -35,18 +36,56 @@ function renderText() {
     }
 }
 
-for (var i = 7; i < 17; i++) {
-    $(".save"+i).text(localStorage.setItem("userInput"+i,$("text"+i).val()));
-}
+var save7 = $(".save7");
+var save8 = $(".save8");
+var save9 = $(".save9");
+var save10 = $(".sav10");
+var save11 = $(".save11");
+var save12 = $(".save12");
+var save13 = $(".save13");
+var save14 = $(".save14");
+var save15 = $(".save15");
+var save16 = $(".save16");
 
-var saveAll = $(".saveAll");
-saveAll.on("click",function() {
-    saveText();
-    renderText();
+save7.on("click",function() {
+    localStorage.setItem("userInput7",$(".text7").val());
+});
+save8.on("click",function() {
+    localStorage.setItem("userInput8",$(".text8").val());
+});
+save9.on("click",function() {
+    localStorage.setItem("userInput9",$(".text9").val());
+});
+save10.on("click",function() {
+    localStorage.setItem("userInput10",$(".text10").val());
+});
+save11.on("click",function() {
+    localStorage.setItem("userInput11",$(".text11").val());
+});
+save12.on("click",function() {
+    localStorage.setItem("userInput12",$(".text12").val());
+});
+save13.on("click",function() {
+    localStorage.setItem("userInput13",$(".text13").val());
+});
+save14.on("click",function() {
+    localStorage.setItem("userInput14",$(".text14").val());
+});
+save15.on("click",function() {
+    localStorage.setItem("userInput15",$(".text15").val());
+});
+save16.on("click",function() {
+    localStorage.setItem("userInput16",$(".text16").val());
+});
+
+var saveAll = $(".saveIcon");
+saveAll.on("click", function () {
+    textSave();
+    renderText(); 
 });
 
 var deleteAll = $(".deleteAll");
-deleteAll.on("click",function() {
+deleteAll.on("click", function () {
     localStorage.clear();
     renderText();
 })
